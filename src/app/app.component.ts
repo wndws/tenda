a69ae91dd352e0300da9a268b24284848b95fdf0
wndws <admin@arigatopool.com>
2024-08-12 07:00:10 +0900

    increase budget

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ClarityModule } from "@clr/angular";
+import { CommonModule } from '@angular/common';
+import { ClarityIcons, fishIcon } from '@cds/core/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ClarityModule,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Tneda';
+  constructor(){
+    ClarityIcons.addIcons(fishIcon);
+  }
 }
