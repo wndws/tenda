b2fe7baae131dd66a9563b7409f29266503d0837
wndws <admin@arigatopool.com>
2024-08-12 08:09:41 +0900

    add cf-turnstile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ClarityModule } from "@clr/angular";
+import { ClarityModule, ClrModalModule } from "@clr/angular";
 import { CommonModule } from '@angular/common';
-import { ClarityIcons, fishIcon } from '@cds/core/icon';
+import { ClarityIcons, envelopeIcon, fishIcon } from '@cds/core/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -10,14 +12,37 @@ import { ClarityIcons, fishIcon } from '@cds/core/icon';
   imports: [
     RouterOutlet,
     ClarityModule,
-    CommonModule
+    CommonModule,
+    ClrModalModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  turnstileUrl: string = "https://challenges.cloudflare.com/turnstile/v0/api.js?onload=onloadTurnstileCallback";
+
+  openModal = false;
+
   title = 'Tneda';
-  constructor(){
-    ClarityIcons.addIcons(fishIcon);
+  constructor(
+    private element: ElementRef
+  ) {
+    ClarityIcons.addIcons(fishIcon, envelopeIcon);
+  }
+
+  ngAfterViewInit() {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = this.turnstileUrl;
+
+    const div = document.getElementById('script');
+    if (div != null) {
+      div.insertAdjacentElement('afterend', script);
+    }
+  }
+
+  email(token: string) {
+    console.log(`Challenge Success ${token}`);
   }
 }
